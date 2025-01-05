c7baee1723ecd5d64d43f4e1eee8d6a3d94e9f7d
artpiazcode <artpiazcode@gmail.com>
2025-01-05 23:24:28 +0000

    Deploying to gh-pages from @ artpiazcode/artpiazcode.github.io@4489c386f03af8969c63571128177dcaf2b8cd3d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-learnings",
-          title: "learnings",
+          title: "Learnings",
           description: "",
           section: "Navigation",
           handler: () => {
