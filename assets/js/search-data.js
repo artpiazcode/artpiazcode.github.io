71c826a7099f82e9bef3348b43c8747fd3c1d688
artpiazcode <artpiazcode@gmail.com>
2025-01-06 03:28:51 +0000

    Deploying to gh-pages from @ artpiazcode/artpiazcode.github.io@8070d2ca1da07e7dd3a14398c0dbdcaa4e8b0f1f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,6 +34,17 @@ ninja.data = [{
           window.location.href = "/blog/2024/ModernBERT/";
         
       },
+    },{id: "post-positionalembeddings",
+      
+        title: "Positionalembeddings",
+      
+      description: "",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2024/PositionalEmbeddings/";
+        
+      },
     },{
         id: 'social-linkedin',
         title: 'LinkedIn',
