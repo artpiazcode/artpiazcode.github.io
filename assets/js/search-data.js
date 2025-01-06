e3f4a8b3fb5c3b0aa11aedc82a5e0921c06eba92
artpiazcode <artpiazcode@gmail.com>
2025-01-06 02:26:47 +0000

    Deploying to gh-pages from @ artpiazcode/artpiazcode.github.io@35b791da8a4a12ebb3f280f079b736181602e349 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-learnings",
           title: "Learnings",
-          description: "",
+          description: "Publishing my scattered notes for future me!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/learnings/index.html";
@@ -31,22 +31,22 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2025/ModernBERT/";
+          window.location.href = "/blog/2024/ModernBERT/";
         
       },
     },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%61%72%74%70%69%61%7A%63%6F%64%65@%67%6D%61%69%6C.%63%6F%6D", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/arpitas", "_blank");
+        },
+      },{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%61%72%74%70%69%61%7A%63%6F%64%65@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
